aed50354dfd328fc5467b2435ea41b05e774afed
PiyumiKawee <piyumikaweesha637@gmail.com>
2026-03-08 20:37:40 +0530

    Employee Details

--- Shanel_ERP/frontend/src/App.jsx
+++ Shanel_ERP/frontend/src/App.jsx
@@ -31,7 +31,9 @@ const App = () => {
             <Route path="/inventory/raw-materials" element={<ProductPage pageTitle="Raw Materials" typeFilter="Raw" />} />
 
             <Route path="/POS" element={<POS />} />
-            <Route path="/hr" element={<div>HR Page</div>} />
+            <Route path="/hr" element={<Hrdashboardpage />} />
+            <Route path="/hr/employees" element={<EmployeesPage pageTitle="Employees" typeFilter="Employees" />} />
+            <Route path="/hr/employee/:id" element={<EmployeeDetail />} />
             <Route path="/finance" element={<div>Finance Page</div>} />
             <Route path="/logout" element={<div>Logout Page</div>} />
           </Routes>
